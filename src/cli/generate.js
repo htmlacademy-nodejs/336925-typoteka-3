0b6cc9037d6beec89d230d1574da063a4a32b10e
Бубликов Павел Сергеевич <pbublikov@phoenixit.ru>
2020-03-26 22:16:01 +0300

    Add data folder and readFile from data

--- src/cli/generate.js
+++ src/cli/generate.js
@@ -1,80 +1,67 @@
-'use strict';
+"use strict";
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
-const {ExitCode} = require(`../constants`);
-const {getRandomNumber, randomDate, shuffle} = require(`../utils`);
+const { ExitCode } = require(`../constants`);
+const { getRandomNumber, randomDate, shuffle } = require(`../utils`);
 
 const DEFAULT_COUNT = 1;
 const MAX_COUNT = 1000;
 const FILE_NAME = `mock.json`;
 const MONTH_LIMIT_AGO = 3;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучше рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят. Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов. Он написал больше 30 хитов. Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения. Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const readContent = async filePath => {
+  try {
+    const content = await fs.readFile(filePath, `utf-8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 const getStartDate = (date = new Date()) => {
   date.setMonth(date.getMonth() - MONTH_LIMIT_AGO);
   return date;
 };
 
-const generateOffers = (count) => {
-  return Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomNumber(0, TITLES.length - 1)],
-    createdDate: randomDate(getStartDate(), new Date()).toLocaleDateString(),
-    announce: shuffle(SENTENCES).slice(0, 5).join(` `),
-    fullText: shuffle(SENTENCES).slice(0, getRandomNumber(1, SENTENCES.length - 1)).join(` `),
-    сategory: shuffle(CATEGORIES).slice(0, getRandomNumber(1, CATEGORIES.length - 1)),
-  }));
+const generateOffers = (count, titles, categories, sentences) => {
+  return Array(count)
+    .fill({})
+    .map(() => ({
+      title: titles[getRandomNumber(0, titles.length - 1)],
+      createdDate: randomDate(getStartDate(), new Date()).toLocaleDateString(),
+      announce: shuffle(sentences)
+        .slice(0, 5)
+        .join(` `),
+      fullText: shuffle(sentences)
+        .slice(0, getRandomNumber(1, sentences.length - 1))
+        .join(` `),
+      сategory: shuffle(categories).slice(
+        0,
+        getRandomNumber(1, categories.length - 1)
+      )
+    }));
 };
 
 module.exports = {
   name: `--generate`,
-  async run(args) {
+  run(args) {
+    const sentencesPromise = readContent(FILE_SENTENCES_PATH);
+    const titlesPromise = readContent(FILE_TITLES_PATH);
+    const categoriesPromise = readContent(FILE_CATEGORIES_PATH);
+
+    const promiseMocks = Promise.all([
+      sentencesPromise,
+      titlesPromise,
+      categoriesPromise
+    ]);
+
     const [count] = args;
     let countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -87,13 +74,19 @@ module.exports = {
       countOffer = DEFAULT_COUNT;
     }
 
-    const content = JSON.stringify(generateOffers(countOffer));
-
-    try {
-      await fs.writeFile(FILE_NAME, content);
-      console.info(chalk.greenBright(`Operation success. File created.`));
-    } catch (error) {
-      console.info(chalk.redBright(`Can't write data to file...`));
-    }
+    promiseMocks
+      .then(([sentences, titles, categories]) =>
+        JSON.stringify(
+          generateOffers(countOffer, titles, categories, sentences)
+        )
+      )
+      .then(async content => {
+        try {
+          await fs.writeFile(FILE_NAME, content);
+          console.info(chalk.greenBright(`Operation success. File created.`));
+        } catch (error) {
+          console.info(chalk.redBright(`Can't write data to file...`));
+        }
+      });
   }
 };
