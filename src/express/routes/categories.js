a6d9ede42fccb37bf2c2eee4cd13dccec68298d6
buba <sas_bub@mail.ru>
2020-10-14 21:04:11 +0300

    Add render templates to route

--- src/express/routes/categories.js
+++ src/express/routes/categories.js
@@ -4,6 +4,6 @@ const {Router} = require(`express`);
 
 const categoriesRouter = new Router();
 
-categoriesRouter.get(`/`, (req, res) => res.send(`/categories`));
+categoriesRouter.get(`/`, (req, res) => res.render(`publications-by-category`));
 
 module.exports = categoriesRouter;
